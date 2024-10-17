367958b2a032e4b47d1d39cc274f29d2fc60cd66
TVlearns <t.akselvinje@gmail.com>
2024-10-17 13:10:03 -0700

    Changed repo name to wacintosh

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/digital-time-capsule/',
+  base: '/wacintosh/',
   build: {
     outDir: 'build',
   },
